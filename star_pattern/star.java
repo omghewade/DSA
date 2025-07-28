6b188784ac6473190eff09792a93e6041a1b6fa1
omghewade <ghewadeom@gmail.com>
2025-07-29 00:26:31 +0530

    new1

---
+++ star_pattern/star.java
@@ -0,0 +1,19 @@
+package star_pattern;
+
+import java.util.*;
+
+class star {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+            System.out.println();
+
+        }
+
+    }
+}
