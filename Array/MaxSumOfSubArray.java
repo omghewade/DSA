db914c4f73c995d22163861dac09edbffc4a16c5
omghewade <ghewadeom@gmail.com>
2025-08-11 23:12:10 +0530

    updated logic

--- Array/MaxSumOfSubArray.java
+++ Array/MaxSumOfSubArray.java
@@ -2,38 +2,34 @@ package Array;
 
 public class MaxSumOfSubArray {
 
+    public static void maxOfSubArray(int arr[]) {
+        int maxx = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
 
-public static  void  maxOfSubArray(int arr[])
-{   
-    int maxx = Integer.MIN_VALUE;
-    for(int i=0; i<arr.length; i++)
-    {       int sum =0;
+            for (int j = i; j < arr.length; j++) {
+                int sum = 0;
 
-        for(int j=i;j<arr.length;j++)
-        {
-            for(int k=i;k<j;k++)
-            {
-                sum = sum + k;
+                for (int k = i; k <= j; k++) {
+                    sum = sum + arr[k];
+                }
+                System.out.println(sum);
+                if (maxx < sum) {
+                    maxx = sum;
+                }
+
             }
-            System.out.println(sum);
-            if(maxx<sum)
-            {
-                maxx = sum;
-            }
-               
         }
+
+        System.out.println("the maximum of sub arrAY IS" + maxx);
+
     }
 
-     System.out.println("the maximum of sub arrAY IS"+ maxx);
-            
-}
-    
     public static void main(String[] args) {
 
-        int arr[] = {-2,10,22,44,-5,-7};
+        int arr[] = { 1, -2, 6, -1, 3 };
 
         maxOfSubArray(arr);
-        
+
     }
-    
+
 }
