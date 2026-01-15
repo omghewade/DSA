9b8144e8d321152a1ee31a3f964f3c9734dc5972
omghewade <ghewadeom@gmail.com>
2026-01-15 13:24:53 +0530

    new code 1

--- Array Rev/pair.java
+++ Array Rev/pair.java
@@ -2,7 +2,7 @@ public class pair {
 
     public static void pair(int arr[]) {
 
-
+          int tp=0 ;
         
 
         for( int i=0; i < arr.length-1;i++)
@@ -11,10 +11,15 @@ public class pair {
             for(int j=i+1; j < arr.length; j++)
             {
                 System.out.print("(" + arr[i] +"+"+ arr[j] +")");
+
+                tp ++;
             }
 
             System.err.println();
         }
+
+
+        System.out.println(tp);
         
     }
 
